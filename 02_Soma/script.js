1d8b35cc5ca873dd9e06a5f538c655a814fa7b80
Kelfer Marques <114634531+KelferM@users.noreply.github.com>
2022-10-25 00:40:39 -0400

    Add files via upload

---
+++ 02_Soma/script.js
@@ -0,0 +1,10 @@
+/* 
+  Solicite 2 números, faça a soma deles
+  e apresente o resultado final ao usuário
+*/
+
+alert("Iremos somar 2 números")
+let numberOne = prompt("Digite o primeiro número: ")
+let nuberTwo = prompt("Digite o Segundo número: ")
+let result = Number(numberOne) + Number(nuberTwo)
+alert("Resultado final: " + result)
